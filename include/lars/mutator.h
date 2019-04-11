512d9bbd2d087bc59ad3f7dd6210c0e753a320d0
Lars Melchior <TheLartians@users.noreply.github.com>
2019-04-11 12:19:08 +0200

    v0.6 (#9)
    * add Werror
    
    * add unnamed dummy bitfield
    
    * v0.6

--- include/lars/mutator.h
+++ include/lars/mutator.h
@@ -10,11 +10,11 @@ namespace lars{
   }
 }
 
-#define MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,GETTER_VISIBILITY,SETTER_VISIBILITY) private: TYPE _ ## NAME DEFAULT; GETTER_VISIBILITY TYPE const &NAME()const{ return _ ## NAME; } SETTER_VISIBILITY template <typename ... Args> auto &set_ ## NAME(const Args & ... args){ auto previous_ ## NAME = ::lars::mutator_helper::initialize<TYPE>(args...); std::swap(previous_ ## NAME, _ ## NAME ); CODE; return *this; }
+#define MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,GETTER_VISIBILITY,SETTER_VISIBILITY) private: TYPE _ ## NAME DEFAULT; GETTER_VISIBILITY TYPE const &NAME()const{ return _ ## NAME; } SETTER_VISIBILITY template <typename ... Args> auto &set_ ## NAME(const Args & ... args){ auto previous_ ## NAME = ::lars::mutator_helper::initialize<TYPE>(args...); std::swap(previous_ ## NAME, _ ## NAME ); CODE; return *this; } unsigned :0
 
-#define MUTABLE_MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,GETTER_VISIBILITY,SETTER_VISIBILITY) private: mutable TYPE _ ## NAME DEFAULT; GETTER_VISIBILITY TYPE const &NAME()const{ return _ ## NAME; } SETTER_VISIBILITY template <typename ... Args> auto &set_ ## NAME(const Args & ... args)const{ auto previous_ ## NAME = ::lars::mutator_helper::initialize<TYPE>(args...); std::swap(previous_ ## NAME, _ ## NAME ); CODE; return *this; }
+#define MUTABLE_MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,GETTER_VISIBILITY,SETTER_VISIBILITY) private: mutable TYPE _ ## NAME DEFAULT; GETTER_VISIBILITY TYPE const &NAME()const{ return _ ## NAME; } SETTER_VISIBILITY template <typename ... Args> auto &set_ ## NAME(const Args & ... args)const{ auto previous_ ## NAME = ::lars::mutator_helper::initialize<TYPE>(args...); std::swap(previous_ ## NAME, _ ## NAME ); CODE; return *this; } unsigned :0
 
-#define REFERENCE_MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,GETTER_VISIBILITY,SETTER_VISIBILITY) private: TYPE * _ ## NAME DEFAULT; GETTER_VISIBILITY TYPE &NAME()const{ return * _ ## NAME; } SETTER_VISIBILITY auto &set_ ## NAME(TYPE &t){ _ ## NAME = &t; CODE; return *this; }
+#define REFERENCE_MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,GETTER_VISIBILITY,SETTER_VISIBILITY) private: TYPE * _ ## NAME DEFAULT; GETTER_VISIBILITY TYPE &NAME()const{ return * _ ## NAME; } SETTER_VISIBILITY auto &set_ ## NAME(TYPE &t){ _ ## NAME = &t; CODE; return *this; } unsigned :0
 
 #define MUTATOR_MEMBER_PRIVATE(TYPE,NAME,DEFAULT,CODE) MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,public:,private:)
 #define MUTATOR_MEMBER_PROTECTED(TYPE,NAME,DEFAULT,CODE) MUTATOR_MEMBER_VISIBILITY(TYPE,NAME,DEFAULT,CODE,public:,protected:)
